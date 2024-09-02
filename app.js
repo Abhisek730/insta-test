e57ff5d2db218abb1b7aeb56c9a43858cbc9692d
Abhishek Agrawal <ashu.agrawalksj@gmail.com>
2024-09-03 03:13:29 +0530

    yee

--- app.js
+++ app.js
@@ -26,11 +26,11 @@ app.get("/test", authorizeUser, (req, res) => {
 })
 
 // Serve static files from React app
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 
 // Handle React routing, return all requests to React app
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, './client/build', 'index.html'));
 });
 
 
